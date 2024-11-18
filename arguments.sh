7964389d388736d54dfc6dad4129f86f7ef639ca
Statute8234 <76547158+Statute8234@users.noreply.github.com>
2024-11-18 16:01:49 -0500

    Add files via upload

--- arguments.sh
+++ arguments.sh
@@ -4,9 +4,10 @@ lines=$(ls -lh $1 | wc -l)
 
 if [ $# -ne 1 ]
 then
-        echo "This script requires exactly one ddirectory path passed to it."
-        echo "Please try again."
-        exit 1
+	echo "This script requires exactly one ddirectory path passed to it."
+	echo "Please try again."
+	exit 1
 fi
 
 echo "You have $(($lines-1)) object in the $1 directory."
+
---
+++ game.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+running=0
+
+while [ $running -ne 1 ]
+do
+	clear
+	echo "___"
+done
